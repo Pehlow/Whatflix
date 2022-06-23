1b39540db261ca17b16a42f0054afac96f892250
jpi <jpi@atolcd.com>
2022-06-23 09:41:52 +0200

    Optimizing fetch

--- src/Find.jsx
+++ src/Find.jsx
@@ -1,6 +1,6 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect, useCallback } from "react";
 import debounce from "lodash.debounce";
-import Mockup from "./Mockup";
+
 function Find() {
   const [media, setMedia] = useState("");
   const [filter, setFilter] = useState("movie");
@@ -8,29 +8,30 @@ function Find() {
 
   const handleChange = (eventQuery) => {
     setMedia(eventQuery.target.value);
-    fetchQuery(eventQuery.target.value);
   };
 
   const toggleFilter = (eventClick) => {
-    console.log(eventClick.target.value);
     setFilter(eventClick.target.value);
-    fetchQuery(media);
   };
 
-  const fetchQuery = useRef(
-    debounce(async (query) => {
-      if (query && query.length > 1) {
+  const fetchQuery = useCallback(
+    debounce(async ({ _media, _filter }) => {
+      if (_media && _media.length > 1) {
         const response = await fetch(
-          `https://api.themoviedb.org/3/search/${filter}?api_key=${
+          `https://api.themoviedb.org/3/search/${_filter}?api_key=${
             import.meta.env.VITE_API_KEY
-          }&query=${query}&language=en-US&page=1&include_adult=false`
+          }&query=${_media}&language=en-US&page=1&include_adult=false`
         );
         const data = await response.json();
         setResults(data.results);
-        console.log(data);
       }
-    }, 250)
-  ).current;
+    }, 250),
+    []
+  );
+
+  useEffect(() => {
+    fetchQuery({ _media: media, _filter: filter });
+  }, [media, filter]);
 
   return (
     <div className="flex flex-col justify-center p-3">
@@ -68,30 +69,31 @@ function Find() {
         </div>
       </div>
       <div className="mt-[7rem] flex flex-col">
-        {results.map((result) => (
-          <div
-            key={result.id}
-            className="flex max-h-32 flex-1 flex-grow-0 items-center rounded p-3"
-          >
-            <img
-              src={
-                result.poster_path
-                  ? `https://image.tmdb.org/t/p/w500/${result.poster_path}`
-                  : "https://via.placeholder.com/500x750"
-              }
-              alt={result.title}
-              className="object-fit max-h-28 w-28 flex-1 grow-0 rounded"
-            />
-            <div className="flex flex-auto flex-col p-2">
-              <h2 className="text-base font-bold text-neutral-50">
-                {result.title || result.name}
-              </h2>
-              <p className="overflow-hidden text-clip text-xs text-neutral-50">
-                {result.overview.substring(0, 150) + "..."}
-              </p>
+        {results &&
+          results.map((result) => (
+            <div
+              key={result.id}
+              className="flex max-h-32 flex-1 flex-grow-0 items-center rounded p-3"
+            >
+              <img
+                src={
+                  result.poster_path
+                    ? `https://image.tmdb.org/t/p/w500/${result.poster_path}`
+                    : "https://via.placeholder.com/500x750"
+                }
+                alt={result.title}
+                className="object-fit max-h-28 w-28 flex-1 grow-0 rounded"
+              />
+              <div className="flex flex-auto flex-col p-2">
+                <h2 className="text-base font-bold text-neutral-50">
+                  {result.title || result.name}
+                </h2>
+                <p className="overflow-hidden text-clip text-xs text-neutral-50">
+                  {result.overview.substring(0, 150) + "..."}
+                </p>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
       </div>
     </div>
   );
