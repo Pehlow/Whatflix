ce5be76729a00bb922917d814a5ec39369c70312
jpi <jpi@atolcd.com>
2022-06-23 11:53:02 +0200

    Scroll snap

--- src/HorizontalMediaList.jsx
+++ src/HorizontalMediaList.jsx
@@ -3,12 +3,12 @@ import React from "react";
 function HorizontalMediaList(props) {
   return (
     <>
-      <h1 className="text-lg font-medium text-neutral-50">{props.title}</h1>
-      <div className="h-52 overflow-y-hidden overflow-x-scroll whitespace-nowrap">
+      <h1 className="text-lg font-medium text-neutral-50 ">{props.title}</h1>
+      <div className="h-52 snap-x overflow-y-hidden overflow-x-scroll whitespace-nowrap ">
         {props.list.map((media) => (
           <div
             key={media.id}
-            className="inline-block h-full rounded p-3 first:pl-0"
+            className="inline-block h-full snap-start -scroll-m-3 rounded p-3 first:scroll-m-0 first:pl-0"
           >
             <img
               src={
