b24fcc5d20b9dcc83b2dc78ec15e3312852291fe
jpi <jpi@atolcd.com>
2022-06-23 12:05:14 +0200

    Home page tweaks

--- src/Home.jsx
+++ src/Home.jsx
@@ -6,10 +6,17 @@ function Home() {
   const movies = Mockup.movies;
   const shows = Mockup.shows;
   return (
-    <div className="grid justify-around p-3">
-      <HorizontalMediaList list={movies} title="Movies" />
-      <HorizontalMediaList list={shows} title="TV Shows" />
-    </div>
+    <>
+      <div className="bg-neutral-800 pt-20">
+        <h1 className="p-3 text-3xl font-medium text-neutral-50">
+          What to watch next ?
+        </h1>
+      </div>
+      <div className="flex flex-col justify-around p-3">
+        <HorizontalMediaList list={movies} title="Movies" />
+        <HorizontalMediaList list={shows} title="TV Shows" />
+      </div>
+    </>
   );
 }
 
--- src/HorizontalMediaList.jsx
+++ src/HorizontalMediaList.jsx
@@ -4,7 +4,7 @@ function HorizontalMediaList(props) {
   return (
     <>
       <h1 className="text-lg font-medium text-neutral-50 ">{props.title}</h1>
-      <div className="h-52 snap-x overflow-y-hidden overflow-x-scroll whitespace-nowrap ">
+      <div className="h-64 snap-x overflow-y-hidden overflow-x-scroll whitespace-nowrap ">
         {props.list.map((media) => (
           <div
             key={media.id}
