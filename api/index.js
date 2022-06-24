3cf98a0c8c227ca14411f710b34dd8008ed45c18
jpi <jpi@atolcd.com>
2022-06-24 16:00:04 +0200

    cors and json on express

--- api/index.js
+++ api/index.js
@@ -1,8 +1,12 @@
 import express from "express";
 import { PrismaClient } from "@prisma/client";
+import cors from "cors";
 
 const prisma = new PrismaClient();
 const app = express();
+
+app.use(cors());
+app.use(express.json());
 /**
  * @route   GET /movies
  * @desc    Get all movies
